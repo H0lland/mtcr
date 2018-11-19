ce8323f9c0695ed325c22100c9424eccc3e21b69
Unknown <matiturner@gmail.com>
2018-11-19 13:27:01 -0500

    Created Scrape.py
    Created scrape.py which combs solution files to determine  what percentage of tasks were pushed to the cloud

---
+++ scrape.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3.7
+
+def main():
+	#read from solution file
+	inName = input("Sol file: ")
+	inFile = open(inName+".sol","r")
+	lines = inFile.readlines()
+	inFile.close()
+
+	#count the number of schedule placements and the number placed on the cloud
+	cloudCnt = 0
+	schedCnt = 0
+	for i in range(len(lines)):
+		line = lines[i].split()
+		#check that this variable was set to 1
+		if line[1] == "1":
+			#get whether placement or schedule
+			varType = line[0].split("[")[0]
+			if varType == "scheduling":
+				schedCnt += 1
+				cloudlet = line[0].split(",")[1]
+				#if on cloud
+				if cloudlet[0] == "4":
+					cloudCnt += 1
+
+	#append data
+	outFile = open("sol.cnt","a+")
+	outFile.write(inName+":\t"+str(cloudCnt)+"/"+str(schedCnt))
+	outFile.close()
+
+main()
